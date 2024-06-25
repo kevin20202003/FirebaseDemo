3d000fc06341f6af8dc5984d12bfa67fe4ffd492
Kevin Chala <kchala900@gmail.com>
2024-06-25 10:13:54 -0500

    Agregar archivos de proyecto.

---
+++ FirebaseDemo/Models/Setting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirebaseDemo.Models
+{
+    public static class Setting
+    {
+        internal const string FireBaseDatabaseUrl = "";
+        internal const string FireBaseSeceret = "";
+    }
+}
